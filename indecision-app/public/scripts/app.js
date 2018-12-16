15c6c30eedd34c0ba371d28b7b5f74b9f85b422e
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-16 12:42:10 +1100

    dynamic jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,8 +1,22 @@
 "use strict";
 
-console.log('=========>This is working<========');
-var template = React.createElement("div", null, React.createElement("h1", null, "Indescion APP"), React.createElement("p", null, "This is some info"), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two"), React.createElement("li", null, "Item Three"), React.createElement("li", null, "Item Four")));
-var templateTwo = React.createElement("div", null, React.createElement("h1", null, "Max Muster"), React.createElement("p", null, "Age: 55"), React.createElement("p", null, "Location: Berlin"));
+console.log('=========> This is working <========');
+var appObj = {
+  title: 'Indescion APP',
+  subtitle: 'The greates descision making App out there'
+};
+var template = React.createElement("div", null, React.createElement("h1", null, appObj.title), React.createElement("p", null, appObj.subtitle), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two"), React.createElement("li", null, "Item Three"), React.createElement("li", null, "Item Four")));
+var user = {
+  name: 'Max Muster',
+  age: 90 // location: 'Berlin'
+
+};
+
+function getLocation(location) {
+  return location ? React.createElement("p", null, "Location: ", location) : undefined;
+}
+
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name), React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.querySelector('#app');
 var appRootTwo = document.querySelector('#appTwo');
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,9 +1,14 @@
-console.log('=========>This is working<========');
+console.log('=========> This is working <========');
+
+const appObj = {
+  title: 'Indescion APP',
+  subtitle: 'The greates descision making App out there'
+};
 
 const template = (
   <div>
-    <h1>Indescion APP</h1>
-    <p>This is some info</p>
+    <h1>{appObj.title}</h1>
+    <p>{appObj.subtitle}</p>
     <ol>
       <li>Item One</li>
       <li>Item Two</li>
@@ -13,15 +18,27 @@ const template = (
   </div>
 );
 
+const user = {
+  name: 'Max Muster',
+  age: 90
+  // location: 'Berlin'
+};
+
+function getLocation(location) {
+  return location ? <p>Location: {location}</p> : undefined;
+}
+
 const templateTwo = (
   <div>
-    <h1>Max Muster</h1>
-    <p>Age: 55</p>
-    <p>Location: Berlin</p>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    {getLocation(user.location)}
   </div>
 );
 
 const appRoot = document.querySelector('#app');
 const appRootTwo = document.querySelector('#appTwo');
+
 ReactDOM.render(template, appRoot);
+
 ReactDOM.render(templateTwo, appRootTwo);
