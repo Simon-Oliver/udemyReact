94e08e2b322876bf3eb5c22544650c356699b95a
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-18 21:13:47 +1100

    Added dynamic rendered list items

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,9 @@
 "use strict";
 
-console.log('=========> This is working <========');
 var appObj = {
   title: 'Indescion APP',
   subtitle: 'The greates descision making App out there',
-  options: ['One', 'Two']
+  options: []
 };
 
 var onFormSubmit = function onFormSubmit(e) {
@@ -13,14 +12,33 @@ var onFormSubmit = function onFormSubmit(e) {
 
   if (option) {
     appObj.options.push(option);
+    e.target.elements.option.value = '';
   }
+
+  renderItems();
 };
 
-var template = React.createElement("div", null, React.createElement("h1", null, appObj.title), appObj.subtitle && React.createElement("p", null, appObj.subtitle), React.createElement("p", null, appObj.options.length > 0 ? 'Here are your options' : 'No options'), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two"), React.createElement("li", null, "Item Three"), React.createElement("li", null, "Item Four")), React.createElement("form", {
-  onSubmit: onFormSubmit
-}, React.createElement("input", {
-  type: "text",
-  name: "option"
-}), React.createElement("button", null, "Add Option")));
+var removeAll = function removeAll() {
+  appObj.options = [];
+  renderItems();
+};
+
 var appRoot = document.querySelector('#app');
-ReactDOM.render(template, appRoot);
+
+var renderItems = function renderItems() {
+  var template = React.createElement("div", null, React.createElement("h1", null, appObj.title), appObj.subtitle && React.createElement("p", null, appObj.subtitle), React.createElement("p", null, appObj.options.length > 0 ? 'Here are your options' : 'No options'), React.createElement("p", null, appObj.options.length), React.createElement("button", {
+    onClick: removeAll
+  }, " Remove All"), React.createElement("ol", null, appObj.options.map(function (e) {
+    return React.createElement("li", {
+      key: e
+    }, e);
+  })), React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), React.createElement("button", null, "Add Option")));
+  ReactDOM.render(template, appRoot);
+};
+
+renderItems();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,35 +1,46 @@
-console.log('=========> This is working <========');
 const appObj = {
   title: 'Indescion APP',
   subtitle: 'The greates descision making App out there',
-  options: ['One', 'Two']
+  options: []
 };
-
 const onFormSubmit = e => {
   e.preventDefault();
   const option = e.target.elements.option.value;
   if (option) {
     appObj.options.push(option);
+    e.target.elements.option.value = '';
   }
+  renderItems();
 };
 
-const template = (
-  <div>
-    <h1>{appObj.title}</h1>
-    {appObj.subtitle && <p>{appObj.subtitle}</p>}
-    <p>{appObj.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-      <li>Item Three</li>
-      <li>Item Four</li>
-    </ol>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  </div>
-);
-
+const removeAll = () => {
+  appObj.options = [];
+  renderItems();
+};
 const appRoot = document.querySelector('#app');
-ReactDOM.render(template, appRoot);
+
+const renderItems = () => {
+  const template = (
+    <div>
+      <h1>{appObj.title}</h1>
+      {appObj.subtitle && <p>{appObj.subtitle}</p>}
+      <p>
+        {appObj.options.length > 0 ? 'Here are your options' : 'No options'}
+      </p>
+      <p>{appObj.options.length}</p>
+      <button onClick={removeAll}> Remove All</button>
+      <ol>
+        {appObj.options.map(e => (
+          <li key={e}>{e}</li>
+        ))}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+renderItems();
