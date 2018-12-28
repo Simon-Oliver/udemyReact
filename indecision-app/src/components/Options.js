b83ad9a862f336b24a2e1eb87744304706defe47
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-28 19:14:29 +1100

    Finished styling

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Option from './Option';
 const Options = props => (
   <>
-    <button className="button button--link" onClick={props.handleDeleteOptions}>
-      Remove All
-    </button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    {props.options.map(e => (
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
+    </div>
+    {props.options.length === 0 && (
+      <p className="widget__message">Please add an option to get started!</p>
+    )}
+    {props.options.map((e, i) => (
       <Option
         key={e}
         optionText={e}
+        count={i + 1}
         handleDeleteOption={props.handleDeleteOption}
       />
     ))}
